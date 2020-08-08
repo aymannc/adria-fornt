d4891467cfc9436675b270ab2fb1a23d995c942b
Ayman Nait Cherif <ayman.nait21@gmail.com>
2020-08-08 02:56:27 +0100

    Adding lazy loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect} from "react";
+import React, {Component, Suspense, useEffect} from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import {useDispatch} from "react-redux";
 import {BrowserRouter} from "react-router-dom";
@@ -6,10 +6,14 @@ import {Redirect, Route, Switch} from "react-router";
 import Auth from "./auth/Auth";
 import {Container} from "react-bootstrap";
 import Header from "./app/Header";
-import AjouterVirment from "./virement/AjouterVirement";
-import SignerVirement, {SignSuccess} from "./virement/SignerVirement";
-import VirementList from './virement/VirementList';
+import {SignSuccess} from "./virement/SignerVirement";
 import * as actions from './auth/AuthAction'
+
+
+const AjouterVirment = React.lazy(() => import('./virement/AjouterVirement'));
+const SignerVirement = React.lazy(() => import('./virement/SignerVirement'));
+const VirementList = React.lazy(() => import('./virement/VirementList'));
+
 
 function App() {
     const dispatch = useDispatch();
@@ -17,22 +21,28 @@ function App() {
         dispatch(actions.authCheckState())
     }, [])
     return (
-
         <BrowserRouter>
             <Header/>
             <Container>
                 <Switch>
                     <Route path='/auth'
                            activeClassName="active" component={Auth}/>
-                    <Route path='/ajouter-virement' component={AjouterVirment}/>
-                    <Route path='/verification' component={SignerVirement}/>
+                    <Route path='/ajouter-virement' component={WaitingComponent(AjouterVirment)}/>
+                    <Route path='/verification' component={WaitingComponent(SignerVirement)}/>
                     <Route path='/success' component={SignSuccess}/>
-                    <Route path='/list-virements' component={VirementList}/>
+                    <Route path='/list-virements' component={WaitingComponent(VirementList)}/>
                     <Redirect to='/list-virements'/>
                 </Switch>
             </Container>
         </BrowserRouter>
-    );
+    )
 }
 
+const WaitingComponent = (Component: any) => {
+    return (props: any) => (
+        <Suspense fallback={<div>Loading...</div>}>
+            <Component {...props} />
+        </Suspense>
+    );
+}
 export default App;
