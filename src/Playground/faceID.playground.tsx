895f812da97f054f4bcacb9021451e6073eb40f8
MohamedOUFTOU <mohamedouftou1998@gmail.com>
2020-08-20 16:29:17 +0100

    Adding alerts with screenShots preview to faceID.playground.tsx

--- src/Playground/faceID.playground.tsx
+++ src/Playground/faceID.playground.tsx
@@ -1,5 +1,5 @@
 import React, {useState} from "react";
-import {Button, Card, Col, Container, Form, Row} from "react-bootstrap";
+import {Alert, Button, Card, Col, Container, Form, Row, Spinner} from "react-bootstrap";
 import Webcam from "react-webcam";
 import "./style.css"
 import axios, {AxiosError} from "axios";
@@ -20,13 +20,35 @@ const Playground = () => {
     )
 };
 
+const CustomAlert = (props:{variant:string, message:string,link:string}) =>{
+    return (
+        <Alert variant={props.variant}>{props.message}{'  '}
+            {props.link !== ' '?<Alert.Link href={props.link}>Capture Link</Alert.Link>:null}
+        </Alert>
+    )
+}
 const Capture = () =>{
+
+    const [screenshot,setScreenShot] = useState({
+        imageUrl:'https://icon-library.com/images/no-image-icon/no-image-icon-0.jpg'
+    })
+    const [alert,setAlert] = useState({
+        variant:"info",
+        message:"Click on Capture to detect the faces present in the webcam !!!",
+        link:' ',
+        loading: false
+    })
+
     const webcamRef = React.useRef(null);
 
     const capture = React.useCallback(
         () => {
             // @ts-ignore
             const imageSrc = webcamRef.current.getScreenshot();
+            setScreenShot(screenshot=>({
+                ...screenshot,
+                imageUrl:imageSrc
+            }))
             faceRec(imageSrc)
         },
         [webcamRef]
@@ -39,15 +61,63 @@ const Capture = () =>{
     const faceRec = async (data: any) => {
         const image = await fetch(data).then(res => res.blob())
         const bodyFormData = new FormData();
+        setAlert(alert => ({
+            ...alert,
+            variant: "warning",
+            message: "The captured image is now loaded to the server for prediction...",
+            link:' ',
+            loading: true
+        }))
         console.log(image)
         bodyFormData.append('file', image,"data.jpg")
         axios.post("http://127.0.0.1:5000/facial_recognition", bodyFormData, {headers: {'Content-Type': 'multipart/form-data'}})
             .then(response => {
                 console.log(response)
+                if(response.data.response){
+                    const  results:any[] = response.data.response
+                    setAlert(alert =>({
+                        ...alert,
+                        variant: "success",
+                        message: response.status+" : Found user with id "+results[0]?.id+" and distance of "+results[0]?.distance,
+                        link: response.data?.image_requested_link,
+                        loading: false
+                    }))
+                } else if(!response.data.error && !response.data.response){
+                    setAlert(alert =>({
+                        ...alert,
+                        variant: "success",
+                        message: response.status+" : The image provided by the webcam did not match any user in the database",
+                        link: response.data?.image_requested_link,
+                        loading: false
+                    }))
+                }
+
             })
             .catch((err:AxiosError) => {
-                console.log(err.response?.data)
+                console.log(err.response)
+                setAlert(alert =>({
+                    ...alert,
+                    variant: "danger",
+                    message: err.response?.statusText+" : "+err.response?.data?.error,
+                    link: err.response?.data?.image_requested_link,
+                    loading: false
+                }))
             })
+    }
+
+    const reset = ()=>{
+        setScreenShot(screenshot=>({
+            ...screenshot,
+            imageUrl:'https://icon-library.com/images/no-image-icon/no-image-icon-0.jpg'
+        }))
+
+        setAlert(alert=>({
+            ...alert,
+            variant:"info",
+            message:"Click on Capture to detect the faces present in the webcam !!!",
+            link:' ',
+            loading: false
+        }))
     }
     return (
         <Col  md={8} className="mt-1">
@@ -55,8 +125,11 @@ const Capture = () =>{
                 <Card.Header as="h5">Predict</Card.Header>
                 <Card.Body>
                     <Card.Title>Take a picture for the model to predict</Card.Title>
-                    <Row>
-                        <Col md={8} xs={8}>
+                    <Row className="justify-content-md-center">
+                        <CustomAlert variant={alert.variant} message={alert.message} link={alert.link}/>
+                    </Row>
+                    <Row className="justify-content-md-center">
+                        <Col md={7} xs={7}>
                             <Webcam
                                 audio={false}
                                 ref={webcamRef}
@@ -66,10 +139,27 @@ const Capture = () =>{
                             />
                         </Col>
                         <Col>
-                            <h5>Resultas :</h5>
+                            <h5>ScreenShoot Preview</h5>
+                            <img src={screenshot.imageUrl} className="screnshot"/>
                         </Col>
                     </Row>
-                    <Button variant="primary" onClick={capture}>Capture</Button>
+                    <Row className="justify-content-md-center mt-1">
+                        <Col>
+                            <Button variant="primary" onClick={capture} disabled={alert.loading}>
+                                {
+                                    alert.loading ? <Spinner
+                                        as="span"
+                                        animation="border"
+                                        size="sm"
+                                        role="status"
+                                        aria-hidden="true"
+                                    /> : null
+                                }
+                                {alert.loading ? ' Loading...' : 'Capture'}
+                            </Button>
+                        </Col>
+                        <Col><Button variant="secondary" onClick={reset}>Reset</Button></Col>
+                    </Row>
                 </Card.Body>
             </Card>
         </Col>
